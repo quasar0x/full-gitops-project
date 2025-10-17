079e51a060aaa7c053bda6d32d84d81d1932971b
quasar0x <100059333+quasar0x@users.noreply.github.com>
2025-10-17 08:19:06 +0100

    updated app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_current_time():
-    current_time = time.strftime("%y-%m-%d %H:%M:%S", time.localtime)
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return f"The current time of the day: {current_time}"
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=8080)
